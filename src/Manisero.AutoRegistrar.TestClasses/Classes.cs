b0faf532e9220fbf66b0fe875cb69067c3e68b65
manisero <michalaniserowicz@gmail.com>
2016-09-05 23:59:11 +0200

    Started implementing 'single dependency -> derived' case

--- src/Manisero.AutoRegistrar.TestClasses/Classes.cs
+++ src/Manisero.AutoRegistrar.TestClasses/Classes.cs
@@ -3,4 +3,11 @@
     public class NoDependencies
     {
     }
+
+    public class DependantOf_NoDependencies
+    {
+        public DependantOf_NoDependencies(NoDependencies noDependencies)
+        {
+        }
+    }
 }
--- src/Manisero.AutoRegistrar.Tests/LifetimeResolutionTests.fs
+++ src/Manisero.AutoRegistrar.Tests/LifetimeResolutionTests.fs
@@ -5,8 +5,14 @@ open FsUnit.Xunit
 open Manisero.AutoRegistrar.TestClasses
 open LifetimeResolution
 
+// test data
+
 let noDepsReg = { defaultRegistration with classType = typedefof<NoDependencies> }
 let noDepsReg_lifetime = { noDepsReg with lifetime = Some 3 }
+
+let depOfNoDepsReg =  { defaultRegistration with classType = typedefof<DependantOf_NoDependencies> }
+
+// helpers
 
 let assertRegLengths expectedResolvedLength expectedRemainingLength output =
     output.resolvedRegistrations.Length |> should equal expectedResolvedLength
@@ -16,9 +22,18 @@ let assertReg expectedClassType expectedLifetime reg =
     reg.classType |> should equal expectedClassType
     reg.lifetime |> should equal expectedLifetime
 
+// tests
+
 [<Fact>]
 let ``no dependencies -> longestLifetime``() = 
     let result = resolveLifetime noDepsReg []
 
     result |> assertRegLengths 1 0
     List.head result.resolvedRegistrations |> assertReg noDepsReg.classType longestLifetime
+
+[<Fact>]
+let ``single dependency -> derived``() = 
+    let result = resolveLifetime depOfNoDepsReg [noDepsReg_lifetime]
+
+    result |> assertRegLengths 1 1
+    List.head result.resolvedRegistrations |> assertReg depOfNoDepsReg.classType noDepsReg_lifetime.lifetime
