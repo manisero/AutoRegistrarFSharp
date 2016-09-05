6c3de3b3ca5270a9713b0500262bafa0e96fc867
manisero <michalaniserowicz@gmail.com>
2016-09-05 23:49:40 +0200

    Refactored LifetimeResolutionTests

--- src/Manisero.AutoRegistrar.Tests/LifetimeResolutionTests.fs
+++ src/Manisero.AutoRegistrar.Tests/LifetimeResolutionTests.fs
@@ -8,14 +8,17 @@ open LifetimeResolution
 let noDepsReg = { defaultRegistration with classType = typedefof<NoDependencies> }
 let noDepsReg_lifetime = { noDepsReg with lifetime = Some 3 }
 
+let assertRegLengths expectedResolvedLength expectedRemainingLength output =
+    output.resolvedRegistrations.Length |> should equal expectedResolvedLength
+    output.remainingRegistrations.Length |> should equal expectedRemainingLength
+
 let assertReg expectedClassType expectedLifetime reg =
     reg.classType |> should equal expectedClassType
     reg.lifetime |> should equal expectedLifetime
 
 [<Fact>]
-let ``no dependencies -> 1``() = 
+let ``no dependencies -> longestLifetime``() = 
     let result = resolveLifetime noDepsReg []
 
-    result.resolvedRegistrations.Length |> should equal 1
-    result.remainingRegistrations.Length |> should equal 0
+    result |> assertRegLengths 1 0
     List.head result.resolvedRegistrations |> assertReg noDepsReg.classType longestLifetime
