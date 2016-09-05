926b85e174f20f3f5df5f9db9453f1287f6cfab6
manisero <michalaniserowicz@gmail.com>
2016-09-05 22:44:34 +0200

    Moved sample tests to other module

--- src/Manisero.AutoRegistrar.Tests/LifetimeResolutionTests.fs
+++ src/Manisero.AutoRegistrar.Tests/LifetimeResolutionTests.fs
@@ -5,20 +5,6 @@ open FsUnit.Xunit
 open Manisero.AutoRegistrar.TestClasses
 open LifetimeResolution
 
-[<Fact>]
-let ``passing test``() = 
-    true |> should equal true
-
-[<Fact>]
-let ``failing test``() = 
-    true |> should equal false
-
-[<Theory>]
-[<InlineData(true)>]
-[<InlineData(false)>]
-let ``theory``(input) = 
-    input |> should equal true
-
 let NoDepsReg = { defaultRegistration with classType = typedefof<NoDependencies> }
 
 [<Fact>]
---
+++ src/Manisero.AutoRegistrar.Tests/SampleTests.fs
@@ -0,0 +1,18 @@
+﻿module SampleTests
+
+open Xunit
+open FsUnit.Xunit
+
+[<Fact>]
+let ``passing test``() = 
+    true |> should equal true
+
+[<Fact>]
+let ``failing test``() = 
+    true |> should equal false
+
+[<Theory>]
+[<InlineData(true)>]
+[<InlineData(false)>]
+let ``theory``(input) = 
+    input |> should equal true
