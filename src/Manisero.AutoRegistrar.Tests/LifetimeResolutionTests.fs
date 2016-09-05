1158f6d5d622e3729893535bb41b53ad7fb1c77b
manisero <michalaniserowicz@gmail.com>
2016-09-05 22:14:49 +0200

    Added sample tests

--- src/Manisero.AutoRegistrar.Tests/LifetimeResolutionTests.fs
+++ src/Manisero.AutoRegistrar.Tests/LifetimeResolutionTests.fs
@@ -1,2 +1,19 @@
 ﻿module LifetimeResolutionTests
 
+open Xunit
+open FsUnit.Xunit
+open LifetimeResolution
+
+[<Fact>]
+let ``passing test``() = 
+    true |> should equal true
+
+[<Fact>]
+let ``failing test``() = 
+    true |> should equal false
+
+[<Theory>]
+[<InlineData(true)>]
+[<InlineData(false)>]
+let ``theory``(input) = 
+    input |> should equal true
