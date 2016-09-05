c3e3f666cbbd23ef28445e328fb0de57588c7657
manisero <michalaniserowicz@gmail.com>
2016-09-05 21:08:52 +0200

    Created solution

---
+++ src/Manisero.AutoRegistrar/Script.fsx
@@ -0,0 +1,7 @@
+﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+
+#load "AutoRegistrar.fs"
+open Manisero.AutoRegistrar
+
+// Define your library scripting code here
