9ecf1c5d48c4a143026adff613770cbcda97d152
manisero <michalaniserowicz@gmail.com>
2016-09-06 23:44:04 +0200

    Updated implementation notes

--- src/Manisero.AutoRegistrar/AutoRegistrar.fs
+++ src/Manisero.AutoRegistrar/AutoRegistrar.fs
@@ -10,31 +10,41 @@ Notes:
   - implemented interface(s) (optional)
 - [nice to have] some registrations may only indicate class's lifetime without specifying implemented interface(s)
 
-DiscoverAndResolve (initialMap -> registrations)
+DiscoverAssemblies (() -> assemblies)
 - get all referenced assemblies (accept some filter?)
-- invoke MapAndResolve(assemblies)
 
-MapAndResolve (initialMap, assemblies -> registrations)
+ResolveDependencies (assemblies, initialMap -> classToInterfaceMap)
 - scan assemblies for classes (accept some filter?)
-- build classToInterfaceMap based on initialMap
+- build classToInterfaceMap
+  - respect initialMap
   - if multiple classes implement given interface, do not register any of those classes as the interface's implementations
-- invoke Resolve(map)
 
-Resolve (classToInterfaceMap, (initialMap?) -> registrations)
-- build dependency tree (actually, a directed acyclic graph, DAG)
-  - node in the tree: class (or registration)
-  - for given class, its dependencies are its parents
-    - dependencies are constructor parameters
-      - if more than one constructor, then exception
-  - assign "dependancy level" to all classes (registrations)
-    - level = depth of the node in the "tree"
-      - for each class with no dependencies, assign 0
-      - then, for each class whose dependencies have level assigned (all of them), assign highest dependency level + 1
-      - repeat above step until all levels are assigned
+BuildDependencyMap (classToInterfaceMap -> dependencyMap)
+- for each class, resolve classes (not interfaces) it depends on
+  - if interface with no implementation encountered, then exception
+
+BuildDependencyTree (dependencyMap -> dependencyTree)
+- (actually not a tree, but a directed acyclic graph, DAG)
+- node in the tree: class (or registration)
+- for given class, its dependencies (classes, not interfaces) are its parents
+  - dependencies are constructor parameters
+    - if more than one constructor, then exception
+
+AssignDependancyLevels (dependencyTree -> dependencyTree)
+- assign "dependancy level" to all classes (registrations)
+- level = depth of the node in the "tree"
+  - for each class with no dependencies, assign 0
+  - then, for each class whose dependencies have level assigned (all of them), assign highest dependency level + 1
+  - repeat above step until all levels are assigned
+
+ResolveLifetimes (dependencyTree, (initialMap?) -> registrations)
 - order classes (registrations) by dependancyLevel
 - for each class (ordered), resolve it's lifetime
   - respect initialMap
   - the class derives its lifetime from its shortest living dependency
   - if no dependencies, then longest possible lifetime
+
+TODO:
+- unify data types accepted / returned by functions
 
 *)
