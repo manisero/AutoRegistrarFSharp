e8b3cfc0c6035eaaf138459a05786bdf41d2d692
manisero <michalaniserowicz@gmail.com>
2016-09-19 23:31:06 +0200

    Started implementing ImplementationMap module

--- src/Manisero.AutoRegistrar/ImplementationMap.fs
+++ src/Manisero.AutoRegistrar/ImplementationMap.fs
@@ -1,2 +1,16 @@
 ﻿module ImplementationMap
 
+let buildImplementationMap initRegs types =
+    // build [type -> implemented interfaces] dict
+    // - first, take it from regs
+    //   - if reg has interfaceTypes filled, take as they are
+    //   - else, detect interfaces
+    // - then, take from types
+    // build registration list from the dict
+    // apply lifetimes from initRegs
+    // return registration list
+    // TODO: ^ rethink, think of efficient building of the result list
+
+    // - for each type, this type implements all implemented interfaces
+    //   - if multiple classes implement given interface, do not register any of those classes as the interface's implementations
+    initRegs
