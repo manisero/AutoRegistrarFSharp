e8163ab782cb38a69d7c5296498f709e6aeb1926
manisero <michalaniserowicz@gmail.com>
2016-09-21 00:16:14 +0200

    ImplementationMap design

--- src/Manisero.AutoRegistrar/ImplementationMap.fs
+++ src/Manisero.AutoRegistrar/ImplementationMap.fs
@@ -1,6 +1,27 @@
 ﻿module ImplementationMap
 
-let buildImplementationMap initRegs types =
+let buildImplementationMap regs =
+    // regs contains all registrations to fill (fill = set interfaceTypes)
+    // each reg can contain lifetime, but should not contain interfaceTypes
+    
+    // for each reg:
+    // - it's interfaces are interfaces and class it implements
+    //   - TODO: consider walking through full type hierarchy
+    // if multiple classes implement given interface, then this interface should not appear in any those classes' interfaceTypes
+    // - e.g. IDisposable can be implemented by multiple classes
+    // if a base class of given class is any reg's classType, then the base class should not appear in the class's interfaceTypes
+
+    // OBSOLETE:
+    // let typeToRegMap = [type -> it's registration map] from initregs
+    //   - keys are classTypes as well as interfaceTypes
+    //   - if dict key conflict, error
+    //
+    // let remainingTypes = types where not typeToRegMap.ContainsKey
+    //
+    // for each concrete class in remainingTypes
+    //   - 
+
+    // OBSOLETE:
     // build [type -> implemented interfaces] dict
     // - first, take it from regs
     //   - if reg has interfaceTypes filled, take as they are
