4ec45ff7d7e7ac62025b1d18f3b3d6637c45712c
manisero <michalaniserowicz@gmail.com>
2016-09-25 21:02:09 +0200

    Removed Script.fsx

--- src/Manisero.AutoRegistrar/Script.fsx
+++
@@ -1,7 +0,0 @@
-﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
-// for more guidance on F# programming.
-
-#load "AutoRegistrar.fs"
-open Manisero.AutoRegistrar
-
-// Define your library scripting code here
