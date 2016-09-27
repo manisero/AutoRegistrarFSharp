92c017b45b7882005eef0ef04fb9d8e6d32d9a1a
manisero <michalaniserowicz@gmail.com>
2016-09-27 22:25:14 +0200

    Started defining AutoRegistrar api

--- src/Manisero.AutoRegistrar/AutoRegistrar.fs
+++ src/Manisero.AutoRegistrar/AutoRegistrar.fs
@@ -1,1 +1,10 @@
-﻿namespace Manisero.AutoRegistrar
+﻿module AutoRegistrar
+
+open DependencyGraph
+open DependancyLevels
+open Lifetimes
+
+let FromImplementationMap regs =
+    regs |> BuildDependencyGraph
+    regs |> AssignDependancyLevels
+    regs |> ResolveLifetimes
