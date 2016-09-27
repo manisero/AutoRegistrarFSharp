116a239754f2c12cd3d200763cba9803cc429937
manisero <michalaniserowicz@gmail.com>
2016-09-27 22:52:37 +0200

    Created IntegrationTests project

---
+++ src/Manisero.AutoRegistrar.IntegrationTests/AutoRegistrarTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Manisero.AutoRegistrar.IntegrationTests
+{
+    public class AutoRegistrarTests
+    {
+        [Fact]
+        public void test()
+        {
+            
+        }
+    }
+}
