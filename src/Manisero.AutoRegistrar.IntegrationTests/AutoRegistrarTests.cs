7766dc17eacc280462dd254cb2efd3d204df749d
manisero <michalaniserowicz@gmail.com>
2016-10-18 23:19:23 +0200

    C# functions now return C# Lists

--- src/Manisero.AutoRegistrar.IntegrationTests/AutoRegistrarTests.cs
+++ src/Manisero.AutoRegistrar.IntegrationTests/AutoRegistrarTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Manisero.AutoRegistrar.Domain;
 using Manisero.AutoRegistrar.TestClasses;
 using Xunit;
 
@@ -13,10 +14,10 @@ namespace Manisero.AutoRegistrar.IntegrationTests
             var testClassesAssembly = typeof(R1).Assembly;
             var ignoredTypes = new HashSet<Type> { typeof(MultiCtors), typeof(SelfDependency), typeof(CyclicDependency1), typeof(CyclicDependency2) };
 
-            var result = AutoRegistrar.FromRootAssemblyCSharp2(typeof(R2),
-                                                               testClassesAssembly,
-                                                               x => x == testClassesAssembly,
-                                                               x => !ignoredTypes.Contains(x));
+            var result = AutoRegistrar.FromRootAssemblyCSharp(new List<Registration> { new Registration(typeof(R2)) { Lifetime = 3 } },
+                                                              testClassesAssembly,
+                                                              x => x == testClassesAssembly,
+                                                              x => !ignoredTypes.Contains(x));
         }
     }
 }
--- src/Manisero.AutoRegistrar/AutoRegistrar.fs
+++ src/Manisero.AutoRegistrar/AutoRegistrar.fs
@@ -1,6 +1,7 @@
 ﻿module Manisero.AutoRegistrar.AutoRegistrar
 
 open System
+open System.Collections.Generic
 open System.Reflection
 open Manisero.AutoRegistrar.Domain
 open AssemblyDiscovery
@@ -10,17 +11,17 @@ open DependencyGraph
 open DependancyLevels
 open Lifetimes
 
-let FromImplementationMapFs regs = regs |> BuildDependencyGraph |> AssignDependancyLevels |> ResolveLifetimes |> List.toSeq
-let FromImplementationMap regs = FromImplementationMapFs (Seq.toList regs)
+let FromImplementationMapFs regs = regs |> BuildDependencyGraph |> AssignDependancyLevels |> ResolveLifetimes
+let FromImplementationMap regs = FromImplementationMapFs (Seq.toList regs) |> List<Registration>
 
 let FromRegistrationsFs regs = regs |> BuildImplementationMap |> FromImplementationMapFs
-let FromRegistrations regs = FromRegistrationsFs (Seq.toList regs)
+let FromRegistrations regs = FromRegistrationsFs (Seq.toList regs) |> List<Registration>
 
 let FromAssembliesFs initRegs typeFilter assemblies = assemblies |> DiscoverRegistrations initRegs typeFilter |> FromRegistrationsFs
-let FromAssemblies initRegs typeFilter assemblies = FromAssembliesFs (Seq.toList initRegs) typeFilter (Seq.toList assemblies)
+let FromAssemblies initRegs typeFilter assemblies = FromAssembliesFs (Seq.toList initRegs) typeFilter (Seq.toList assemblies) |> List<Registration>
 
 let FromRootAssemblyFs initRegs rootAssembly assemblyFilter typeFilter = rootAssembly |> DiscoverAssemblies assemblyFilter |> FromAssembliesFs initRegs typeFilter
-let FromRootAssembly initRegs rootAssembly assemblyFilter typeFilter = FromRootAssemblyFs (Seq.toList initRegs) rootAssembly assemblyFilter typeFilter
+let FromRootAssembly initRegs rootAssembly assemblyFilter typeFilter = FromRootAssemblyFs (Seq.toList initRegs) rootAssembly assemblyFilter typeFilter |> List<Registration>
 
 let FromRootAssemblyCSharp initRegs rootAssembly (assemblyFilter:Converter<Assembly, bool>) (typeFilter:System.Converter<Type, bool>) =
     let assemblyFilterFs = FSharpFunc.FromConverter assemblyFilter
